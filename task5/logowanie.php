71e07720118ed1887e44d0e36171f410b294177d
Krzysztof Charlikowski <kryzyyspl@gmail.com>
2020-04-15 14:41:06 +0200

    Fix small bug in logowanie.php

--- task5/logowanie.php
+++ task5/logowanie.php
@@ -10,24 +10,22 @@
 		{
 			$_SESSION["zalogowanyImie"] = $osoba1->imieNazwisko;
 			$_SESSION["zalogowany"] = 1;
-			echo "hello adam";
 			header("Location: user.php");
 		}
 		else if ($ulogin == $osoba2->login && $upassword == $osoba2->haslo)
 		{
 			$_SESSION["zalogowanyImie"] = $osoba2->imieNazwisko;
 			$_SESSION["zalogowany"] = 1;
-			echo "hello agata";
 			header("Location: user.php");
 		}
 		else
 		{
 			$_SESSION["zalogowanyImie"] = "";
 			$_SESSION["zalogowany"] = 0;
-			echo "hello noone";
 			header("Location: index.php");
 		}
 		//echo "Przesłany login: $ulogin </br>";
 		//echo "Przesłane hasło: $upassword </br>";
 	}
+	header("Location: index.php");
 ?>
