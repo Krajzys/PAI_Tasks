59a0f6e785bd79717e781a676131ae0dac1537aa
Krzysztof Charlikowski <kryzyyspl@gmail.com>
2020-05-22 03:27:05 +0200

    Add full project folder from PAI Task9(ASP.NET)

---
+++ task9/PAI9/PAI9/Models/MusicDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PAI9.Models
+{
+    public class MusicDbContext: DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public ICollection<Song> Collection { get; set; }
+
+        public System.Data.Entity.DbSet<PAI9.Models.Genre> Genres { get; set; }
+    }
+}
